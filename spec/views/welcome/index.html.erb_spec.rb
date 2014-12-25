66968f18d79fa68aaa1a0378455866aa64316f77
Kirill Pimenov <kirill@pimenov.cc>
2014-12-25 21:56:04 +0100

    Renders estimations

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "welcome/index.html.erb", :type => :view do
-  # it 'should contain user\'s private chains'
+  it 'should contain user\'s private estimations' do
+    estimations = (1..3).map { FactoryGirl.create(:estimation) }
+    assign(:estimations, estimations)
+
+    render
+
+    expect(rendered.scan('<li>').size).to eq(3)
+    estimations.each do |estimation|
+      expect(rendered).to have_text(estimation.title)
+    end
+  end
 end
