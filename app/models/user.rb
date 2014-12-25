3fa22035dcf17b3f1e28af24ae484c8ef66fe026
Kirill Pimenov <kirill@pimenov.cc>
2014-12-25 15:00:38 +0100

    Added annotate gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  email               :string           default(""), not null
+#  remember_created_at :datetime
+#  sign_in_count       :integer          default("0"), not null
+#  current_sign_in_at  :datetime
+#  last_sign_in_at     :datetime
+#  current_sign_in_ip  :string
+#  last_sign_in_ip     :string
+#  created_at          :datetime
+#  updated_at          :datetime
+#  provider            :string
+#  uid                 :string
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_provider  (provider)
+#  index_users_on_uid       (uid)
+#
+
 class User < ActiveRecord::Base
   devise :omniauthable, :rememberable, :trackable, :omniauth_providers => [:google_oauth2]
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# == Route Map
+#
+#                  Prefix Verb     URI Pattern                            Controller#Action
+# user_omniauth_authorize GET|POST /users/auth/:provider(.:format)        users/omniauth_callbacks#passthru {:provider=>/google_oauth2/}
+#  user_omniauth_callback GET|POST /users/auth/:action/callback(.:format) users/omniauth_callbacks#:action
+#        new_user_session GET      /sign_in(.:format)                     devise/sessions#new
+#    destroy_user_session GET      /sign_out(.:format)                    devise/sessions#destroy
+#                    root GET      /                                      welcome#login
+#
+
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
@@ -6,59 +16,4 @@ Rails.application.routes.draw do
   end
   
   root 'welcome#login'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
