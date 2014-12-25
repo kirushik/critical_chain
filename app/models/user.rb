19e705f69be73c96ade346db446d76ef1a4912e4
Kirill Pimenov <kirill@pimenov.cc>
2014-12-25 14:51:53 +0100

    Correctly signs up with Google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   devise :omniauthable, :rememberable, :trackable, :omniauth_providers => [:google_oauth2]
+
+  def self.from_omniauth auth, current_user = nil
+    return current_user if current_user
+    
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      # user.name = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+    end
+
+  end
 end
