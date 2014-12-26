80b871b28166587c16deb159a694d85bbc802ee7
Kirill Pimenov <kirill@pimenov.cc>
2014-12-26 19:02:22 +0100

    403 if not in development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   # Exception to ensure out indices are authorized with scope
   after_action :verify_policy_scoped, :only => :index
 
-  rescue_from Pundit::NotAuthorizedError, with: :permission_denied
+  rescue_from Pundit::NotAuthorizedError, with: :permission_denied unless Rails.env.development?
 
   def new_session_path scope
     new_user_session_path
