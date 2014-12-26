39f4a1ae73eee539ddc8b715bda9b5ccf191332f
Kirill Pimenov <kirill@pimenov.cc>
2014-12-26 16:26:12 +0100

    Failing feature and Pundit installation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   helper :application
 
   before_action :authenticate_user!
+  include Pundit
 
   def new_session_path scope
     new_user_session_path
---
+++ spec/features/user_permissions_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature "UserPermissions", :type => :feature do
+  scenario 'I shouldn\'t be allowed to view other people estimations' do
+    user_a = FactoryGirl.create(:user)
+    user_b = FactoryGirl.create(:user_with_estimations)
+    others_estimation = user_b.estimations.first
+
+    login_as user_a
+
+    visit estimation_path(others_estimation)
+    expect(page.status_code).to be(401)
+  end
+end
