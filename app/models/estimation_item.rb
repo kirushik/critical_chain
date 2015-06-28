ab20127962479392d6d1eae9ec1d3a9717a6123c
Kirill Pimenov <kirill@pimenov.cc>
2015-06-28 14:09:06 +0200

    Updated annotations

--- app/models/estimation_item.rb
+++ app/models/estimation_item.rb
@@ -8,6 +8,7 @@
 #  estimation_id :integer
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
+#  fixed         :boolean          default(FALSE), not null
 #
 # Indexes
 #
