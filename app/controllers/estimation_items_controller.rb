623f972c2a05c848cbdeca764fb2769111e64b46
Kirill Pimenov <kirill@pimenov.cc>
2015-07-01 19:18:17 +0200

    Inline editing of actual_value

--- app/controllers/estimation_items_controller.rb
+++ app/controllers/estimation_items_controller.rb
@@ -53,6 +53,6 @@ class EstimationItemsController < ApplicationController
 
   private
   def estimation_item_params
-    params.require(:estimation_item).permit(:title, :value, :quantity, :fixed)
+    params.require(:estimation_item).permit(:title, :value, :quantity, :fixed, :actual_value)
   end
 end
--- spec/features/tracking_mode_spec.rb
+++ spec/features/tracking_mode_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 feature "Tracking mode", :type => :feature do
   let(:user) { FactoryGirl.create(:user_with_nonempty_estimations, n: 2) }
   let(:estimation) { user.estimations.first }
+  let(:tracking_mode_estimation) { FactoryGirl.create(:estimation_with_items, tracking_mode: true, user: user) }
 
   before(:each) do
     login_as user
-    visit estimation_path(estimation)
   end
 
   scenario 'I can enter tracking mode', :js do
+    visit estimation_path(estimation)
+
     expect(page).to have_css('.toggle-tracking .fa-toggle-off')
     expect(page).to have_no_css('.toggle-tracking .fa-toggle-on')
 
@@ -17,5 +19,13 @@ feature "Tracking mode", :type => :feature do
 
     expect(page).to have_css('.toggle-tracking .fa-toggle-on')
     expect(page).to have_no_css('.toggle-tracking .fa-toggle-off')
+  end
+
+  scenario 'I can enter value for an item of tracking-mode estimation', :js do
+    visit estimation_path(tracking_mode_estimation)
+
+    page.first('span.editable.actual_value').click
+    page.find('.editable-inline .editable-input input').set 10
+    page.find('.editable-inline .editable-submit').click
   end
 end
