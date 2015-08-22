ffd1d85456b3f8a6176e754be74954dfde9321fc
Kirill Pimenov <kirill@pimenov.cc>
2015-08-22 00:01:53 -0500

    Cleaned up some markup

--- spec/views/estimations/_estimation.html.erb_spec.rb
+++ spec/views/estimations/_estimation.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "estimations/_estimation.html.erb", :type => :view do
   it 'should render estimation\'s calculated values' do
     estimation = FactoryGirl.create(:estimation_with_items, items: {size: 1, count: 4}).decorate
-    
+
     render partial: "estimations/estimation", locals: { estimation: estimation }
 
     expect(rendered).to have_text('4 + 2 = 6')
