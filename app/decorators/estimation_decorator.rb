7d53637f8016dd69ca085c4943b4e2ad870103c3
Kirill Pimenov <kirill@pimenov.cc>
2015-08-21 23:41:03 -0500

    Better partials handling

--- app/decorators/estimation_decorator.rb
+++ app/decorators/estimation_decorator.rb
@@ -21,14 +21,6 @@ class EstimationDecorator < Draper::Decorator
     object.total.to_s(:rounded, significant: true).gsub(/\.0+$/, '') rescue "0"
   end
 
-  def items_partial_name
-    if tracking_mode?
-      'estimation_items/estimation_item_trackable'
-    else
-      'estimation_items/estimation_item'
-    end
-  end
-
   def actual_sum
     object.estimation_items.sum(:actual_value)
   end
