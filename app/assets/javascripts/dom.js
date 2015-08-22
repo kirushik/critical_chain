63390117ac97ee198ec460317de27d4fea6abdbb
Kirill Pimenov <kirill@pimenov.cc>
2015-08-21 19:53:04 -0500

    Highlighting quickfix

--- app/assets/javascripts/dom.js
+++ app/assets/javascripts/dom.js
@@ -4,4 +4,8 @@ Element.prototype.remove = function() {
 
 HTMLElement.prototype.highlight = function() {
   this.classList.add('highlight');
+  // FIXME There should be a better way of not highlighting all previously triggered elements
+  this.addEventListener("animationend", function() {
+    this.classList.remove('highlight');
+  });
 }
