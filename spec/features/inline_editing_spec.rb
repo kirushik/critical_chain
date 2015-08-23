46187dfa7067c7c6b1337ff87f91ce9845805adb
Kirill Pimenov <kirill@pimenov.cc>
2015-08-23 13:11:10 -0600

    Failed attempt to make feature specs work

--- spec/features/inline_editing_spec.rb
+++ spec/features/inline_editing_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 feature "AdditionOfEstimationsAndItems", :type => :feature do
   let(:user) { FactoryGirl.create(:user_with_nonempty_estimations) }
   let(:estimation) { user.estimations.first }
+  let(:estimation_item) { estimation.estimation_items.first }
 
+  let(:title_field_id) { "\#estimation_item_#{estimation_item.id}_title" }
+  let(:old_estimation_title) { estimation_item.title }
   let(:new_estimation_title) { 'Kapandlya' }
+
+  let(:value_field_id) { "\#estimation_item_#{estimation_item.id}_value" }
+  let(:old_estimation_value) { estimation_item.value }
   let(:new_estimation_value) { 7 }
-  
-  let(:old_estimation_value) { estimation.estimation_items.first.value }
 
   before(:each) do
     login_as user
@@ -15,50 +19,53 @@ feature "AdditionOfEstimationsAndItems", :type => :feature do
   end
 
   scenario 'I can modify the estimation item title', :js do
-    expect(page).not_to have_text new_estimation_title
+    title_input = page.find("input#{title_field_id}")
+    expect(title_input.value).to eq old_estimation_title
 
-    page.find('span.editable.title', text: estimation.estimation_items.first.title).click
-    expect(page).to have_css('.editable-inline')
-
-    page.find('.editable-inline .editable-input input').set new_estimation_title
-    page.find('.editable-inline .editable-submit').click
+    title_input.set new_estimation_title
+    title_input.native.send_keys(:return)
 
     wait_for_ajax
-
-    expect(page).to have_text new_estimation_title
+    expect(page.find("input#{title_field_id}").value).to eq new_estimation_title
 
     visit current_path
-    expect(page).to have_text new_estimation_title
+    expect(page.find("input#{title_field_id}").value).to eq new_estimation_title
   end
 
   scenario 'I can modify estimation item value', :js do
-    expect(page).not_to have_text new_estimation_value
+    value_input = page.find("input#{value_field_id}")
+    expect(value_input.value).to eq old_estimation_value
 
-    page.find('span.editable.value', text: estimation.estimation_items.first.value).click
-    page.find('.editable-inline .editable-input input').set new_estimation_value
-    page.find('.editable-inline .editable-submit').click
+    value_input.set new_estimation_value
+    value_input.native.send_keys(:return)
 
     wait_for_ajax
 
-    expect(page).to have_text new_estimation_value
+    expect(page.find("input#{value_field_id}")).to eq new_estimation_value
+    expect(page).to have_text '7 + 7 = 14'
+
+    visit current_path
+
+    expect(page.find("input#{value_field_id}")).to eq new_estimation_value
     expect(page).to have_text '7 + 7 = 14'
   end
 
   scenario 'AJAX-added items are editable', :js do
-
     fill_in 'estimation_item_value', with: new_estimation_value
     fill_in 'estimation_item_title', with: new_estimation_title
     click_button 'Add estimation item'
 
     wait_for_ajax
 
-    expect(page).to have_css('span.editable.value', count: estimation.estimation_items.count)
-    expect(page).to have_css('span.editable.title', count: estimation.estimation_items.count)
-    expect(page).to have_css('span.editable.quantity', count: estimation.estimation_items.count)
+    new_item_id = estimation.reload.estimation_items.last.id
 
-    page.find('span.editable', text: estimation.estimation_items.last.value).click
-    
-    expect(page).to have_css('.editable-inline')
+    expect(page).to have_css('input.editable.value', count: estimation.estimation_items.count)
+    expect(page).to have_css('input.editable.title', count: estimation.estimation_items.count)
+    expect(page).to have_css('input.editable.quantity', count: estimation.estimation_items.count)
+
+    expect(page.evaluate_script("document.getElementById('estimation_item_#{new_item_id}_title').onblur != null"))
+    expect(page.evaluate_script("document.getElementById('estimation_item_#{new_item_id}_value').onblur != null"))
+    expect(page.evaluate_script("document.getElementById('estimation_item_#{new_item_id}_quantity').onblur != null"))
   end
 
   scenario 'I can mark estimation item as fixed', :js do
@@ -68,9 +75,10 @@ feature "AdditionOfEstimationsAndItems", :type => :feature do
   end
 
   scenario 'I can set the number for a batch', :js do
-    page.find('span.editable.quantity').click
-    page.find('.editable-inline .editable-input input').set '4'
-    page.find('.editable-inline .editable-submit').click
+    page.find('input.editable.quantity').set '4'
+    page.find('input.editable.quantity').native.send_keys(:return)
+
+    wait_for_ajax
 
     expect(page).to have_text "#{4*old_estimation_value} + #{2*old_estimation_value} = #{6*old_estimation_value}"
   end
